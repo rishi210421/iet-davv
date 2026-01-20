ade12c80d99f1f29f6e15f5d897ce048017424d1
Mahek Yadav <mahekyadav325@gmail.com>
2026-01-20 17:01:37 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,6 +4,9 @@ const nextConfig = {
   images: {
     domains: ['firebasestorage.googleapis.com'],
   },
-}
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
